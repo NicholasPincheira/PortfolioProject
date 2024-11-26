fd5dbc423a52de17384b644a78e66978f7f499c6
NPincheiraGino <npincheira@gino.cl>
2024-11-26 14:08:06 -0300

    configuracion para subir a git-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Carpeta donde se generará el build
+    sourcemap: false, // Desactiva los sourcemaps para producción
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
